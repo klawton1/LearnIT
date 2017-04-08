d719cebf0a8568cb4055d396016b92172f112f59
klawton1 <klawton1@gmail.com>
2017-04-08 16:05:47 -0700

    made routes for courses and controller action

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,10 @@
 class CoursesController < ApplicationController
+  def search
+    query = params[:q]
+    @courses = Course.search(query)
+  end
+
+  def show
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'users#home'
+
+  get '/search/:q', to: 'courses#search', as: 'search'
+  get '/courses/:id', to: 'courses#show', as: 'course'
   
 end
