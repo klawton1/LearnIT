779a9db868ec23c4f2e8ea6fcd3feeaab4c967c0
klawton1 <klawton1@gmail.com>
2017-04-10 16:15:08 -0700

    created interets table for users courses. fixed rake file to reindex at the end not inside loop

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
   searchkick
   enum provider: [:Coursera, :Udacity, :edX, :Udemy]
+  has_many :interests, dependent: :destroy
+  has_many :users, through: :interests
 end
