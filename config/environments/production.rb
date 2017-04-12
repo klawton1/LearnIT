550f74c38154217c806e30a96794e42912f94dca
klawton1 <klawton1@gmail.com>
2017-04-12 11:55:43 -0700

    secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
