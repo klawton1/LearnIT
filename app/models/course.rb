98217ef2862696938bdfb7cae05ec117c9a9e56a
klawton1 <klawton1@gmail.com>
2017-04-11 21:11:29 -0700

    added views and category to course model, added category to rake tasks

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,6 @@ class Course < ApplicationRecord
   enum provider: [:Coursera, :Udacity, :edX, :Udemy]
   has_many :interests, dependent: :destroy
   has_many :users, through: :interests
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410220551) do
+ActiveRecord::Schema.define(version: 20170412021422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20170410220551) do
     t.string   "duration"
     t.integer  "provider"
     t.boolean  "has_cert"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "category"
+    t.integer  "views",       default: 0
   end
 
   create_table "interests", force: :cascade do |t|
